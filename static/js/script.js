8c2d56b802c631d349e7ef174b87f1512b8277a9
Gurimarie <gurimarie@gmail.com>
2022-03-09 23:37:57 +0000

    Add input-fields to add_shoes-form

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
   $('.sidenav').sidenav({edge: "right"});
   $('.tooltipped').tooltip();
   $('select').formSelect();
+  $('.datepicker').datepicker({
+      format: "dd mmm, yyyy",
+      yearRange: 1,
+      showClearBtn: true,
+      autoClose: true,
+      i18n: {
+        done: "select"
+      }
+  });
 });
