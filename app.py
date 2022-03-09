f3e31ecdd5a3a08c63b09f7e9a3d87140acfeefb
Gurimarie <gurimarie@gmail.com>
2022-03-09 13:56:20 +0000

    Add register-template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -31,6 +32,12 @@ def get_shoes():
     """ Retrieve all shoes for Home-page """
     shoes = mongo.db.shoes.find()
     return render_template("shoes.html", shoes=shoes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
 
 
 # Run app
