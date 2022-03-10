0d3db63cc9b258fdcd842de5d09773ffe50af66c
Gurimarie <gurimarie@gmail.com>
2022-03-10 05:49:25 +0000

    Add manage categories-template

--- app.py
+++ app.py
@@ -171,6 +171,12 @@ def delete_shoes(shoes_id):
     return redirect(url_for('profile', username=session['user']))
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 # Run app
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
